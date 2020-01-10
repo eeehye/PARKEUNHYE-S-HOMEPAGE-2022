f6289816b49bb96dc84898222cfa3b9e6b74e1a2
eeehye <58320648+eeehye@users.noreply.github.com>
2020-01-10 18:01:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,48 +1,60 @@
-
-  
-  /* 포폴 메뉴 */
+/* 포폴 메뉴 */
 $(document).ready(function () {
-    $('.web').click(function(){
-      $('.menu ul li').removeClass('show');
-      $(this).removeClass('show');
-      $('.po_wrap').removeClass('show');
-      $('.po_wrap.t1').addClass('show');
-      $('.web').addClass('show');
+
+  // 고정 메뉴
+  $(window).scroll(function(){
+    var menu_top = document.querySelector('#main_gnb').offsetTop;
+    var win_scroll = document.documentElement.scrollTop;
+    if(win_scroll > menu_top){
+      $('#main_gnb').addClass('fixed');
+    } else {
+      $('#main_gnb').removeClass('fixed');
+    }
+
+    console.log(menu_top, win_scroll);
+    
+  })
+
+  $('.homepage').click(function () {
+    $('.menu ul li').removeClass('show');
+    $(this).removeClass('show');
+    $('.po_wrap').removeClass('show');
+    $('.po_wrap.t1').addClass('show');
+    $('.homepage').addClass('show');
   });
-  $('.mobile').click(function(){
+  $('.web').click(function () {
     $('.menu ul li').removeClass('show');
     $(this).removeClass('show');
     $('.po_wrap').removeClass('show');
     $('.po_wrap.t2').addClass('show');
-    $('.mobile').addClass('show');
-});
-  $('.homepage').click(function(){
+    $('.web').addClass('show');
+  });
+  $('.mobile').click(function () {
     $('.menu ul li').removeClass('show');
     $(this).removeClass('show');
     $('.po_wrap').removeClass('show');
     $('.po_wrap.t3').addClass('show');
-    $('.homepage').addClass('show');
-});
-  $('.sns').click(function(){
+    $('.mobile').addClass('show');
+  });
+  $('.sns').click(function () {
     $('.menu ul li').removeClass('show');
     $(this).removeClass('show');
     $('.po_wrap').removeClass('show');
     $('.po_wrap.t4').addClass('show');
     $('.sns').addClass('show');
-});
-  $('.book').click(function(){
+  });
+  $('.book').click(function () {
     $('.menu ul li').removeClass('show');
     $(this).removeClass('show');
     $('.po_wrap').removeClass('show');
     $('.po_wrap.t5').addClass('show');
     $('.book').addClass('show');
-});
-  $('.project').click(function(){
+  });
+  $('.project').click(function () {
     $('.menu ul li').removeClass('show');
     $(this).removeClass('show');
     $('.po_wrap').removeClass('show');
     $('.po_wrap.t6').addClass('show');
     $('.project').addClass('show');
+  });
 });
-});
-
